6ddc7773dddab771f5f97710865873958d466148
Cole French <16979554+ColeFrench@users.noreply.github.com>
2019-01-09 19:30:50 -0500

    Add tank drive

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -10,13 +10,11 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-/**
- * An example command.  You can replace me with your own command.
- */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+public class TankDrive extends Command {
+  public TankDrive() {
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.m_subsystem);
+    // eg. requires(chassis);
+    requires(Robot.drivetrain);
   }
 
   // Called just before this Command runs the first time
@@ -27,6 +25,7 @@ public class ExampleCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.drivetrain.tankDrive(Robot.oi.getLeftY(), Robot.oi.getRightY());
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -38,11 +37,13 @@ public class ExampleCommand extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.drivetrain.tankDrive(0, 0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.commands.TankDrive;
+
+public class Drivetrain extends Subsystem {
+    private final int LEFT_LEADER_TALON = 1;
+    private final int LEFT_FOLLOWER_TALON = 2;
+    private final int RIGHT_LEADER_TALON = 3;
+    private final int RIGHT_FOLLOWER_TALON = 4;
+
+    private final WPI_TalonSRX leftLeader = new WPI_TalonSRX(LEFT_LEADER_TALON);
+    private final WPI_TalonSRX leftFollower = new WPI_TalonSRX(RIGHT_LEADER_TALON);
+    private final WPI_TalonSRX rightLeader = new WPI_TalonSRX(LEFT_FOLLOWER_TALON);
+    private final WPI_TalonSRX rightFollower = new WPI_TalonSRX(RIGHT_FOLLOWER_TALON);
+
+    public Drivetrain(){
+        leftFollower.follow(leftLeader);
+        rightFollower.follow(rightLeader);
+    }
+
+    @Override
+    public void initDefaultCommand() {
+        setDefaultCommand(new TankDrive());
+    }
+
+    public void tankDrive(double leftSpeed, double rightSpeed) {
+        leftLeader.set(leftSpeed);
+        rightLeader.set(rightSpeed);
+    }
+}
