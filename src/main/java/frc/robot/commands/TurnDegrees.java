784e14d47fa0d54200c2d6456ed2f464acd7a3c0
Henry Prickett-Morgan <hprickett-morgan2019@columbiasecondary.org>
2019-01-12 14:02:35 -0500

    Add TurnDegrees

--- src/main/java/frc/robot/commands/TurnDegrees.java
+++ src/main/java/frc/robot/commands/TurnDegrees.java
@@ -10,38 +10,20 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
-import frc.robot.subsystems.Drivetrain;
 
-public class DriveFeet extends Command {
-  //Inches per Foot * Ticks per Rotation/(Wheel Diameter * pi)
-  public static final double FEET_TO_TICKS = 12 * 4096 / Math.PI / 6;
-  public static final double kP = 1/8;
-  public static final double kI = 0;
-  public static final double kD = 0;
-  public static final double proportionalHeading = 0.05;
-  
-  private final boolean holdHeading;
-  private final double initialHeading;
+public class TurnDegrees extends Command {
+  public static final double p = 1/90;
+  public static final double i = 0;
+  public static final double d = 0;
 
-  private final Drivetrain.LinearOutput linearOutput = Robot.drivetrain.getLinearOutput();
-  private final PIDController pidController = new PIDController(kP, kI, kD, Robot.encoders, linearOutput);
+  private final PIDController pidController = new PIDController(p, i, d, Robot.gyro, Robot.drivetrain.getTurnOutput());
 
-  public DriveFeet(double distance) {
+  public TurnDegrees(double degrees) {
+    requires(Robot.drivetrain);
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
-    this(distance, true);
-  }
-  
-  public DriveFeet(double distance, boolean holdHeading){
-    requires(Robot.drivetrain);
-    this.holdHeading = holdHeading;
 
-    //Set up PIDController and sensors
-    Robot.encoders.zeroEncoders();
-    this.initialHeading = Robot.gyro.getYaw();
-
-    //Set setpoint and enable
-    pidController.setSetpoint(distance * FEET_TO_TICKS);
+    pidController.setSetpoint(Robot.gyro.getYaw() + degrees);
     pidController.enable();
   }
 
@@ -53,9 +35,6 @@ public class DriveFeet extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    if(holdHeading){
-      linearOutput.setHeadingCorrection(proportionalHeading * (initialHeading - Robot.gyro.getYaw()));
-    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -68,7 +47,7 @@ public class DriveFeet extends Command {
   @Override
   protected void end() {
     pidController.disable();
-    Robot.drivetrain.tankDrive(0,0);
+    Robot.drivetrain.tankDrive(0, 0);
   }
 
   // Called when another command which requires one or more of the same
