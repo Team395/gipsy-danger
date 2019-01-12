a02af2d1ed089ee458491217c330b318f3b1a89d
Henry Prickett-Morgan <hprickett-morgan2019@columbiasecondary.org>
2019-01-12 13:30:58 -0500

    Fix DriveFeet so it halts properly

--- src/main/java/frc/robot/commands/DriveFeet.java
+++ src/main/java/frc/robot/commands/DriveFeet.java
@@ -54,18 +54,22 @@ public class DriveFeet extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    linearOutput.setHeadingCorrection(proportionalHeading * (initialHeading - Robot.gyro.getYaw()));
+    if(holdHeading){
+      linearOutput.setHeadingCorrection(proportionalHeading * (initialHeading - Robot.gyro.getYaw()));
+    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return pidController.onTarget();
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    pidController.disable();
+    Robot.drivetrain.tankDrive(0,0);
   }
 
   // Called when another command which requires one or more of the same
