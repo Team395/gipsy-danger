b0bbf919253caf9abe8328cb1d2479623c9fdfe7
Henry Prickett-Morgan <hprickett-morgan2019@columbiasecondary.org>
2019-01-12 12:20:46 -0500

    Add Gyro and Encoders

---
+++ src/main/java/frc/robot/subsystems/DrivetrainGyro.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+
+public class DrivetrainGyro implements PIDSource{
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public static final int GYRO_TALON = 5;
+  private static final int YAW_INDEX = 0;
+
+  private final PigeonIMU pigeon = new PigeonIMU(new TalonSRX(GYRO_TALON));
+
+  public double getYaw(){
+    double[] returnArray = new double[3];
+    pigeon.getYawPitchRoll(returnArray);
+    return returnArray[YAW_INDEX];
+  }
+
+  @Override
+  public PIDSourceType getPIDSourceType() {
+    return PIDSourceType.kDisplacement;
+  }
+
+  @Override
+  public void setPIDSourceType(PIDSourceType pidSource) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public double pidGet(){
+    return getYaw();
+  }
+}
