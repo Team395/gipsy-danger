a261dd974d6ef978d4f2c8be2a929baf40e9bdb1
Cole French <16979554+ColeFrench@users.noreply.github.com>
2019-01-13 14:47:52 -0500

    Add target dimensions

--- src/main/java/frc/robot/utils/Targets.java
+++ src/main/java/frc/robot/utils/Targets.java
@@ -3,6 +3,16 @@ package frc.robot.utils;
 import com._2train395.limelight.api.Target;
 
 public class Targets {
+    // All constants are in either inches or radians.
+    private static final double TAPE_LENGTH = 5.5;
+    private static final double TAPE_WIDTH = 2.0;
+    private static final double TAPE_ANGLE = Math.toRadians(14.5);
+    private static final double TAPE_SEPARATION = 8.0;
+    private static final double TARGET_LENGTH = (2.0 * (TAPE_WIDTH * Math.cos(TAPE_ANGLE))
+            + (TAPE_LENGTH * Math.sin(TAPE_ANGLE))) + TAPE_SEPARATION;
+    private static final double TARGET_WIDTH = (TAPE_LENGTH * Math.cos(TAPE_ANGLE))
+            + (TAPE_WIDTH * Math.sin(TAPE_ANGLE));
+
     /**
      * @param target
      * @param targetHeight the height in feet of the center of the target from the
