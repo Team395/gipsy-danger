1e5276f6fd71d59987169f14a66c6fb6182e9115
HPrickettMorgan <hprickett-morgan2019@columbiasecondary.org>
2019-01-13 12:33:33 -0500

    Fix earlier PR

--- src/main/java/frc/robot/SpeedControllerMap.java
+++ src/main/java/frc/robot/SpeedControllerMap.java
@@ -2,9 +2,17 @@ package frc.robot;
 
 import java.util.HashMap;
 import java.util.Map;
-import 
-public class SpeedControllerMap {
-    Map<Integer, WPI_TalonSRX> talonMap = new HashMap<>();
 
-    public SpeedControllerMap
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class SpeedControllerMap {
+    private Map<Integer, WPI_TalonSRX> talonMap = new HashMap<>();
+
+    public SpeedControllerMap() {
+
+    }
+
+    public WPI_TalonSRX getTalonByID(int id) {
+        return talonMap.get(id);
+    }
 };
