cc9df942bcc590c0deb26438c98d0dabf47a373f
Henry Prickett-Morgan <hprickett-morgan2019@columbiasecondary.org>
2019-01-23 12:21:09 -0500

    Create SparkMAX.java and add interface

---
+++ src/main/java/frc/robot/utils/SparkMAX.java
@@ -0,0 +1,66 @@
+package frc.robot.utils;
+
+import com.revrobotics.*;
+import java.util.*;
+
+public class SparkMAX {
+
+    public SparkMAX(int deviceID) {
+
+    }
+
+    public void set(double output) {
+
+    }
+
+    public void set(double output, ControlType controlType) {
+
+    } 
+
+    public double get() {
+        return 0;
+    }
+
+    public ControlType getControlType() {
+        return null;
+    }
+
+    public void setPIDSetpoint(double setpoint, int slot, ControlType controlType) {
+
+    }
+
+    public double getPIDSetpoint() {
+        return 0;
+    }
+
+    public double getPIDError() {
+        return 0;
+    }
+
+    public void setPIDSlot(double p, 
+                           double i, 
+                           double d, 
+                           double ff, 
+                           double iZone, 
+                           double minOutput, 
+                           double maxOutput, 
+                           int slot) {
+
+    }    
+
+    public void setPIDSlot(List<Double> params, int slot) {
+
+    }
+
+    public List<Double> getPIDSlot(int slot) {
+        return null;
+    }
+
+    public PIDTuner getTuner() {
+        return null;
+    }  
+
+    public void disable() {
+
+    }
+}
