16786e8084eb612b86a33ff11ec1a0f071761a20
Henry <hlewispm2@gmail.com>
2019-01-29 20:17:41 -0500

    Improve documentation

--- src/main/java/frc/robot/utils/SparkMAX.java
+++ src/main/java/frc/robot/utils/SparkMAX.java
@@ -7,9 +7,13 @@ import edu.wpi.first.wpilibj.Timer;
 import java.util.*;
 
 public class SparkMAX {
+
+    //Integral Members of the Controller
     CANSparkMax spark;
     CANPIDController pidController;
     CANEncoder encoder = null;
+
+    //Bookkeeping variables for PID
     ControlType controlType = ControlType.kVoltage;
     double setpoint = 0;
     double zeroPosition = 0;
@@ -21,13 +25,19 @@ public class SparkMAX {
     double lastSampleTime = 0;
     int sampleIndex = 0;
 
+    //Instance of PIDTuner to return
     PIDTuner tuner = new REVPIDTuner("SPARK" + spark.getDeviceId(), pidController);
 
+    /*
+     *  Constructs a new SparkMAX, assuming an encoder only if the motor type is brushless
+     */
     public SparkMAX(int deviceID, MotorType motorType) {
-        //If no value for encoder is provided, assume there is an encoder only if it's brushless
         this(deviceID, motorType, motorType == MotorType.kBrushless);
     }
 
+    /*
+     * Overload of previous constructor to support a brushed motor with an encoder
+     */
     public SparkMAX(int deviceID, MotorType motorType, boolean encoderConnected) {
         //Construct the spark
         spark = new CANSparkMax(deviceID, motorType);
@@ -41,37 +51,56 @@ public class SparkMAX {
         }
     }
 
+    /*
+     *  Implements a set method similar to the WPILIB interface that truncates 
+     *  input to [-1,1]. Controls by scaling closed loop voltage control from 
+     *  0 to 12 rather than 0 to VCC.
+     */
     public void set(double output) {
-        //Cut down to [-1, +1] like in the wpilib 
         output = Math.max(output, 1);
         output = Math.min(output, -1);
 
-        //Basically equivalent to duty cycle control, should be a bit more reliable
         pidController.setReference(output * 12, ControlType.kVoltage);
         //TODO: Test that this isn't affected by PID parameters
         controlType = ControlType.kVoltage;
     }
 
+    /*
+     *  Implements a get method similar to the WPILIB interface, to get the
+     *  current applied output from -1 to 1. Is not necessarily equivalent to
+     *  the set() value.
+     */
+
     public double get() {
         return spark.getAppliedOutput();
     }
 
+    /*
+     *  Returns the last used controlType
+     */
+
     public ControlType getControlType() {
         return controlType;
     }
+
+    /*
+     *  Sets the setpoint for the internal PID controller and enables it.
+     */
 
     public void setPIDSetpoint(double setpoint, int slot, ControlType controlType) {
         //Make sure this is actually PID
         if(controlType != ControlType.kPosition && controlType != ControlType.kVelocity)
             throw new IllegalStateException("Invalid ControlType: Not PID");
         
-        //Bookkeeping. Grr that we have to do this
         this.controlType = controlType;
         this.setpoint = setpoint;
 
-        //Actually set CANPIDController
         pidController.setReference(setpoint, controlType, slot);
     }
+
+    /*
+     *  Returns the last setpoint given to the PIDController.
+     */ 
 
     public double getPIDSetpoint() {
         if(controlType != ControlType.kPosition && controlType != ControlType.kVelocity)
@@ -79,6 +108,10 @@ public class SparkMAX {
 
         return setpoint;
     }
+
+    /*
+     *  Returns the current distance from the setpoint.
+     */
 
     public double getPIDError() {
         if(controlType != ControlType.kPosition && controlType != ControlType.kVelocity)
@@ -90,6 +123,10 @@ public class SparkMAX {
             return setpoint - getVelocity();
         }
     }
+
+    /*
+     *  Sets all PID parameters for a slot one by one. To be used infrequently.
+     */
 
     public void setPIDSlot(double p, 
                            double i, 
@@ -107,7 +144,10 @@ public class SparkMAX {
         pidController.setOutputRange(minOutput, maxOutput, slot);
     }    
 
-    //Here if we want to get, modify, and reset using getPIDSlot()
+    /*
+     *  Overload of previous method to take a List<Double> instead.
+     */
+
     public void setPIDSlot(List<Double> params, int slot) {
         assert params.size() == 7 : "Wrong number of parameters";
 
@@ -121,6 +161,10 @@ public class SparkMAX {
                    slot);
     }
 
+    /*
+     *  Returns the parameters in a given slot.
+     */
+
     public List<Double> getPIDSlot(int slot) {
         return Arrays.asList(pidController.getP(slot),
                              pidController.getI(slot),
@@ -131,19 +175,36 @@ public class SparkMAX {
                              pidController.getOutputMax(slot));
     }
 
+    /*
+     *  Returns an object which can be used to tune the CANPIDController from
+     *  the Preferences utility.
+     */
+
     public PIDTuner getTuner(String name) {
         return tuner;
     }  
+
+    /*
+     *  Turns off the controller and resets the setpoint to 0.
+     */
 
     public void disable() {
         setpoint = 0;
         set(0);
     }
 
+    /*
+     *  Returns the current zeroed encoder position.
+     */
+
     public double getPosition() {
         assert encoder != null : "No encoder connected";
         return encoder.getPosition() - zeroPosition;
     }
+
+    /*
+     *  Returns the current smoothed velocity.
+     */
 
     public double getVelocity() {
         assert encoder != null : "No encoder connected";
@@ -179,8 +240,11 @@ public class SparkMAX {
         return sum/validSamples;
     }
 
+    /*
+     *  Zeroes the position via a member variable.
+     */
+
     public void zeroPosition() {
-        //GRRRR that we have to do this. Bad REV!
         if(encoder == null)
             throw new NoEncoderException();
 
