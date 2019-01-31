a95cf4b6b1e04cbc9b646fd64d5ccceb384078e0
Henry <hlewispm2@gmail.com>
2019-01-30 19:49:01 -0500

    Add shifting to TankDrive.java

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -26,6 +26,12 @@ public class TankDrive extends Command {
   @Override
   protected void execute() {
     Robot.drivetrain.tankDrive(Robot.oi.getLeftY(), Robot.oi.getRightY());
+    
+    if(Robot.oi.getShiftHigh())
+      Robot.drivetrain.shiftHigh();
+
+    else if(Robot.oi.getShiftLow())
+      Robot.drivetrain.shiftLow();
   }
 
   // Make this return true when this Command no longer needs to run execute()
