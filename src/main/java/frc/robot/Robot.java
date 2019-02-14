d16af02aae794c59d6b853375170e6501a50ba36
Henry Prickett-Morgan <hprickett-morgan2019@columbiasecondary.org>
2019-02-13 21:55:15 -0500

    fix oi member variable to align with our convention

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static OI m_oi;
+  public static OI oi;
 
   /**
    * This function is run when the robot is first started up and should be
@@ -22,7 +22,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_oi = new OI();
+    oi = new OI();
   }
 
   /**
