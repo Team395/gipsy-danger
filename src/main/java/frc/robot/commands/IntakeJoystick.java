c08fb738ce489685282d4bab9e0a3ca9ee76c1ae
Henry <hlewispm2@gmail.com>
2019-02-18 19:25:36 -0500

    Remove extra whitespace

--- src/main/java/frc/robot/commands/IntakeJoystick.java
+++ src/main/java/frc/robot/commands/IntakeJoystick.java
@@ -31,7 +31,7 @@ public class IntakeJoystick extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;  
+    return false;
   }
 
   // Called once after isFinished returns true
