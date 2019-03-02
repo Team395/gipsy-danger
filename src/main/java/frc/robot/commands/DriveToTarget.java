0711537dd8a931a3711701e64bcde2979887611a
Henry <hlewispm2@gmail.com>
2019-03-02 15:17:26 -0500

    change lowTargetHeight to targetHeight in distance calculation

--- src/main/java/frc/robot/commands/DriveToTarget.java
+++ src/main/java/frc/robot/commands/DriveToTarget.java
@@ -121,7 +121,7 @@ public class DriveToTarget extends Command {
             xOffset = contour.tx;
             double yOffset = contour.ty;
             
-            double distanceFeet = (lowTargetHeight - cameraHeight) / 
+            double distanceFeet = (targetHeightgi - cameraHeight) / 
                         Math.tan(Math.toRadians(cameraAngle + yOffset)) / 12;
 
             //Set the setpoint for distance relative to current position
