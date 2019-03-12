42182199cba6c95dd62b8ded9575b157c2bf8f90
Henry <hlewispm2@gmail.com>
2019-03-12 18:15:13 -0400

    Add one shot climb sequence

---
+++ src/main/java/frc/robot/commands/OneShotClimb.java
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class OneShotClimb extends CommandGroup {
+
+  public OneShotClimb() {
+    setInterruptible(false);
+    //TODO
+    //addSequential() intake climber mode
+    addSequential(new LevelRobot());
+    addParallel(new CommandGroup() {
+      {
+        addSequential(new DriveDownClimber());
+        addSequential(new DriveForwardClimber());
+      }
+    });
+
+    addSequential(new RetractToSafeDistance());
+  }
+}
