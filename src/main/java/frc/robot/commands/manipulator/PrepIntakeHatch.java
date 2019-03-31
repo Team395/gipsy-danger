fdfcad6f6d54e82ea9cb7d5dd74dc5f0bf65b414
Henry Prickett-Morgan <hprickett-morgan2019@columbiasecondary.org>
2019-03-31 11:23:41 -0400

    fix namespacing and initial implementation of PrepIntakeHatch

--- src/main/java/frc/robot/commands/manipulator/PrepIntakeHatch.java
+++ src/main/java/frc/robot/commands/manipulator/PrepIntakeHatch.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.command.InstantCommand;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 import frc.robot.Robot;
+import frc.robot.subsystems.HatchManipulator;
 
 /**
  * Prepares the intake to take in a hatch.
@@ -12,13 +13,13 @@ public class PrepIntakeHatch extends InstantCommand {
 
   public PrepIntakeHatch() {
     super();
-    requires(Robot.manipulator);
+    requires(Robot.hatchManipulator);
   }
 
   // Called once when the command executes
   @Override
   protected void initialize() {
-
+    Robot.hatchManipulator.setHatchSolenoid(Value.kForward);
   }
 
 }
