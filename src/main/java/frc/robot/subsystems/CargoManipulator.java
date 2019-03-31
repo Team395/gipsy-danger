228143d9cf795196fcd8014cbbc44c55cd64e6c3
Henry Prickett-Morgan <hprickett-morgan2019@columbiasecondary.org>
2019-03-31 11:21:26 -0400

    clean up imports in CargoManipulator

--- src/main/java/frc/robot/subsystems/CargoManipulator.java
+++ src/main/java/frc/robot/subsystems/CargoManipulator.java
@@ -10,17 +10,13 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 import frc.robot.commands.manipulator.IntakeJoystick;
-import frc.robot.enums.IntakeMode;
 
 public class CargoManipulator extends Subsystem {
-	
+
 	WPI_TalonSRX intakeController = Robot.speedControllerMap.getTalonByID(RobotMap.intakeRollerTalonID);
 	
 	//Controls the roller
