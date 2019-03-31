70a93bbf2caf89b31b3c57f4cb2ebf49afaa2d66
Henry Prickett-Morgan <hprickett-morgan2019@columbiasecondary.org>
2019-03-31 11:20:59 -0400

    first implementation of HatchManipulator

--- src/main/java/frc/robot/subsystems/HatchManipulator.java
+++ src/main/java/frc/robot/subsystems/HatchManipulator.java
@@ -1,21 +1,24 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+import frc.robot.RobotMap;
 
 public class HatchManipulator extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
 
-  @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
-  }
+	DoubleSolenoid hatchSolenoid = new DoubleSolenoid(
+		RobotMap.hatchSolenoidForwardChannelID,
+		RobotMap.hatchSolenoidReverseChannelID
+	);
+
+	public void setHatchSolenoid(Value value) {
+		hatchSolenoid.set(value);
+	}
+
+	@Override
+	public void initDefaultCommand() {
+
+	}
 }
