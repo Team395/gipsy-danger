d910c9172b33011991f1d083c0804b55d602b617
Henry Prickett-Morgan <hprickett-morgan2019@columbiasecondary.org>
2019-03-31 11:14:49 -0400

    clean up RobotMap naming

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,22 +14,18 @@ public class RobotMap {
     public static final int driveShiftLowChannelID = 0;  
     public static final int driveShiftHighChannelID = 1; 
 
-    //Intake
-    public static final int intakeRollerTalonID = 10;  
-    public static final int vacuumPumpTalonID = 9;  
-    public static final int suctionValveRelayChannel = 0;  
-    public static final int deployPopoutChannel = 4;  
-    public static final int retractPopoutChannel = 5;  
-    public static final int deployFloorChannel = 3;  
-    public static final int retractFloorChannel = 2;  
-    public static final int lockManipulatorChannel = 7;  
-    public static final int unlockManipulatorChannel = 6;  
-    
+    //Roller Manipulator
+    public static final int intakeRollerTalonID = 10;
+
+    //Hatch Manipulator
+    public static final int hatchSolenoidForwardChannelID = 1;
+    public static final int hatchSolenoidReverseChannelID = 0;
+
     //Climber
     public static final int climberLeadScrewTalonID = 7;  
     public static final int climberWheelPodTalonID = 6;  
 
     //Pigeon
-    public static final int pigeonIMUID = 0;
+    public static final int pigeonIMUTalonID = 0;
 }
  
